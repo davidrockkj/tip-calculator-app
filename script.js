0ec5e66097de249017d28fa1221256d9cd8546cd
davidrockkj <davidrockkj@gmail.com>
2023-08-31 19:19:49 -0300

    Arrumando lógica do input bill

--- script.js
+++ script.js
@@ -1,34 +1,75 @@
-let displayBill = document.getElementById("#display-bill");
-let displayPeople = document.getElementById("#display-people");
-let tipValue = document.getElementById("#tip-value");
-let totalValue = document.getElementById("#total-value");
+let displayBill = document.getElementById("display-bill");
+let displayPeople = document.getElementById("display-people");
+let tipValue = document.getElementById("tip-value");
+let totalValue = document.getElementById("total-value");
 
-const btn1 = document.getElementById("#btn1");
-const btn2 = document.getElementById("#btn2");
-const btn3 = document.getElementById("#btn3");
-const btn4 = document.getElementById("#btn4");
-const btn5 = document.getElementById("#btn5");
-const btn6 = document.getElementById("#btn6");
-const reset = document.getElementById("#reset");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const reset = document.getElementById("reset");
 
-function activeReset() {
-    if (bill > 0) {
+
+displayBill.addEventListener("input", billFunc);
+displayPeople.addEventListener("input", peopleFunc)
+
+// displayBill.value = "0.0";
+displayPeople.value = "1";
+tipValue.innerHTML = "$" + (0.0).toFixed(2);
+totalValue.innerHTML = "$" + (0.0).toFixed(2);
+
+let billValue = 0.0;
+let peopleValue = 1;
+
+reset.disabled = true;
+
+function billFunc() {
+    billValue = parseFloat(displayBill.value);
+    console.log(billValue)
+
+    if (displayBill.value > 0.00) {
         reset.disabled = false;
         reset.classList.remove("btn-noclick");
+        console.log("btn-noclick removido");
+    } else {
+        displayBill.value = "";
+        reset.disabled = true;
+        reset.classList.add("btn-noclick");
+        console.log("btn-noclick removido");
     }
 }
 
-displayBill.innerHTML = ""
 
-function getVal() {
-    var bill = document.querySelector("display-bill").value;
-    console.log(bill);
+
+function peopleFunc() {
+    if (peopleValue === 0) {
+
+    }
 }
 
-function verValor() {
-    valor = bill;
-    var texto = valor.toLocaleString("en-US", 
-        { style: "currency" , currency:"USD"});
 
-    console.log(texto);
-}
+
+
+// function activeReset() {
+//     if (bill > 0) {
+//         reset.disabled = false;
+//         reset.classList.remove("btn-noclick");
+//     }
+// }
+
+// displayBill.innerHTML = ""
+
+// function getVal() {
+//     let bill = document.getElementById("display-bill").value;
+//     console.log(bill);
+// }
+
+// function verValor() {
+//     valor = bill;
+//     var texto = valor.toLocaleString("en-US", 
+//         { style: "currency" , currency:"USD"});
+
+//     console.log(texto);
+// }
